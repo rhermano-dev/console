d90ce3a90c72b5c3994d87b99498a813acabb9c2
Reynaldo Hermano <rhermano@asi-dev5.com>
2023-10-12 15:33:32 +0800

    commit calculator exercise

---
+++ ConsoleApp1/ConsoleApp1/Classes/Calculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    public class Calculator
+    {
+        public int PerformOperation(string operation, int a, int b)
+        {
+            int result = 0;
+
+            if (operation == "add")
+            {
+                result = a + b;
+            }
+            else if (operation == "subtract")
+            {
+                result = a - b;
+            }
+            else if (operation == "multiply")
+            {
+                result = a * b;
+            }
+            else if (operation == "divide")
+            {
+                if (b != 0)
+                {
+                    result = a / b;
+                }
+                else
+                {
+                    throw new DivideByZeroException("Division by zero is not allowed.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid operation.");
+            }
+
+            return result;
+        }
+
+        public string GetOperationDescription(string operation)
+        {
+            switch (operation)
+            {
+                case "add":
+                    return "Addition";
+                case "subtract":
+                    return "Subtraction";
+                case "multiply":
+                    return "Multiplication";
+                case "divide":
+                    return "Division";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        public void LoopAndPrint(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                switch (i % 3)
+                {
+                    case 0:
+                        Console.WriteLine($"Iteration {i}: Multiple of 3");
+                        break;
+                    case 1:
+                        Console.WriteLine($"Iteration {i}: Remainder 1");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Iteration {i}: Remainder 2");
+                        break;
+                }
+            }
+        }
+    }
+}
--- ConsoleApp1/TestProject1/UnitTest1.cs
+++ ConsoleApp1/TestProject1/UnitTest1.cs
@@ -3,30 +3,47 @@ using Microsoft.VisualStudio.TestPlatform.TestHost;
 using ConsoleApp1;
 using Program = ConsoleApp1.Program;
 using PersonDetails = ConsoleApp1.PersonDetails;
+using ConsoleApp1.Classes;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
-
         [Fact]
-        public void Test1()
+        public void LoopAndPrint_ShouldPrintCorrectMessages()
         {
-            // Arrange
-            var program = new Program();
-            var personDetails = new PersonDetails
+            Calculator calculator = new Calculator();
+
+            // Redirect console output to capture the messages
+            using (var sw = new StringWriter())
             {
-                Name = "John",
-                Address = "123 Main St",
-                Age = 30,
-                Gender = "M"
-            };
+                Console.SetOut(sw);
+                calculator.LoopAndPrint(5);
+                string output = sw.ToString();
 
-            // Act
-            bool result = program.Function(personDetails);
+                List<string> list = output.Split(new string[] { "\r\n" }, StringSplitOptions.None).ToList();
 
-            // Assert
-            Assert.False(result);
+
+                //Complete the list depending on the output
+                List<string> toCompare = new List<string> {
+                    "Iteration 0: Multiple of 3"
+                };
+
+                foreach(string data in list)
+                {
+                    if(!string.IsNullOrEmpty(data))
+                    {
+                        try
+                        {
+                            Assert.Equal(data, toCompare.ElementAt(list.IndexOf(data)));
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
     }
